5ee23e05c5743594c14ac7302e3f65e5e170987f
Derek2222 <111035925+Derek2222@users.noreply.github.com>
2022-09-13 17:38:11 -0700

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,3 +2,18 @@
 
 /**
  * _islower - prints
+ *
+ *
+ * @c: int to be checked
+ *
+ *
+ *Return: 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
