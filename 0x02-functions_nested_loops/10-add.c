802f5dea3a1a745cb8e1b045c2db224c047e811d
Derek2222 <111035925+Derek2222@users.noreply.github.com>
2022-09-13 18:09:49 -0700

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,14 @@
-shsdhdhdhsdh
+#include "main.h"
+
+/**
+ * add - add two args and return result
+ * @a: int to add
+ * @b: int to add
+ *
+ * Return: result of addition of arguments
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
