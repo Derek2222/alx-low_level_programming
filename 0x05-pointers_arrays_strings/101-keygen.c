2472f239dc4ac413607f6292a64570c48506bb4d
Ademola Emmanuel <111035925+Derek2222@users.noreply.github.com>
2022-09-19 14:50:49 -0700

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,23 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(void)
+int main()
 {
-  int n;
-  int a[5];
-  int *p;
-
-  a[2] = 1024;
-  p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-  *(p + 5) = 98;
-  /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+    int i = 0;
+    int n = 0;
+    int randomizer = 0;
+    srand((unsigned int)(time(NULL)));
+    char numbers [] = "1234567890";
+    char letter [] = "abcdefghijklmnoqprstuvwyzx";
+    char letterr [] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+    char symbols [] = "!@#$%^&*(){}[]:<>?,./";
+    printf("\nHow long password:");
+    scanf("%d", &n);
+    char password[n];
+    randomizer = rand() % 4;
+    for (i=0;i<n;i++)
+    {
+        if(randomizer == 1)
+        {
+            password[i] = numbers[rand() % 10];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else if (randomizer == 2)
+        {
+            password[i] = symbols[rand() % 26];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else if (randomizer == 3)
+        {
+            password[i] = letterr[rand() % 26];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else
+        {
+            password[i] = letter[rand() % 21];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+    }
+    return main();
 }
